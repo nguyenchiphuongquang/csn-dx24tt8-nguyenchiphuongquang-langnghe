442b36c46c614534bea64a2387e2fc76e6eb7dfc
nguyenchiphuongquang <quangncp2711802@tvu-onschool.edu.vn>
2026-09-01 19:00:54 +0700

    Add files via upload

--- progress-report/scr/config.php
+++ progress-report/scr/config.php
@@ -1,4 +1,12 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'db_langnghe_vietnam');
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+$host = 'localhost';
+$user = 'root';
+$pass = ''; // M?c d?nh XAMPP không có m?t kh?u
+$db_name = 'db_langnghe_vietnam'; // Tên database dã t?o t? file SQL
+
+$conn = new mysqli($host, $user, $pass, $db_name);
+if ($conn->connect_error) {
+    die("K?t n?i CSDL th?t b?i: " . $conn->connect_error);
+}
+$conn->set_charset("utf8mb4");
 ?>
